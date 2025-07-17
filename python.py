d2619c6903826d451f339bd16ea295ef7cbe07b8
Pramanik4 <poojapramanick2001@gmail.com>
2025-07-18 05:07:44 +1200

    git commands

--- python.py
+++ python.py
@@ -1,1 +1,19 @@
 print("bliss git add")
+
+'''got to the project folder 
+  482  git init
+  483  git branch -m pooja
+  484  git add .
+  485  git commit -m "added all file 1st times"
+  488  git config --global user.email "poojapramanick2001@gmail.com"
+  489  git config --global user.name "Pramanik4"
+  490  git commit -m "added all files 1st time"
+  508  git remote set-url origin https://github.com/Pramanik4/git_github_practice.git
+  509  git add .
+  510  git commit -m "added files"
+  511  git push origin pooja
+  after intial setup for each change do this
+  512  git add .
+  513  git commit -m "added a python file"
+  514  git push -u origin pooja
+    '''
